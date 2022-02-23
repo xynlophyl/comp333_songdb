360bf3b9b099509a2518794093f453c737ccd719
xynlophyl <nlin2001lfc@gmail.com>
2022-02-22 22:03:46 -0500

    added register user function and updated song retreival function

--- phptest.php
+++ phptest.php
@@ -17,43 +17,61 @@
         }
         
         // creating new account and passwords
+        if(isset($_REQUEST["register"])){
+            $reg_value = "";
+            $s_username = $_REQUEST["username"];
+            $s_password = $_REQUEST["password"];
+
+            if(!empty($s_username) && !empty($s_password)){
+                $sql_query = "INSERT INTO users(username, password) VALUES ('$s_username', '$s_password')";
+                $res = mysqli_query($conn, $sql_query);
+                $reg_value = "registered $s_username";
+            } else {
+                $reg_value = "values not inputted correctly";
+            }
+
+        }
 
         // generating songs by username
-        if(isset($_REQUEST["Retrieve"])){
-            $out_value = "";
-            $s_username = $_REQUEST[""];
+        if(isset($_REQUEST["retrieve"])){
+            $ret_value = "";
+            $s_username = $_REQUEST["username"];
             
             if(!empty($s_username)) {
-                $sql_query = "SELECT * FROM ratings WHERE username = ('$s_username)";
-                $res = mysql_query($conn, $sql_query);
-                $row = mysqli_fetch_assoc($result);
-                echo $row;
-                $out_value = "$row[song] -> $row[rating]";
+                $sql_query = "SELECT * FROM ratings WHERE username = ('$s_username')";
+                $res = mysqli_query($conn, $sql_query);
+                while ($row = mysqli_fetch_assoc($res)){
+                    $ret_value = $ret_value . "$row[song] -> $row[rating] ";
+                }
+                // $ret_value = "$row[song] -> $row[rating]";
             }
             else { 
-                $out_value = "No songs available!";
+                $ret_value = "Username Invalid!";
             }
         }
         $conn->close();
     ?>
 
+    <!-- form for creating new account -->
     <form method="GET" action="">
-        Username: <input type="text" name="username" placeholder="Enter Username" /><br>
-        <input type="submit" name="register" value="Submit"/>
-        <p><?php 
-            if(!empty($out_value)){
-            echo $out_value;
-            }
-        ?></p>
+    Username: <input type="text" name="username" placeholder="Enter" /><br>
+    Password: <input type="password" name="password" placeholder="Enter Password" /><br>
+    <input type="submit" name = "register" value= "Submit"/>
+    <p><?php
+        echo $reg_value;
+    ?></p>
     </form>
 
-    <form method="GET" action="">
+    <!-- form for getting user's songs -->
+
+    <form method="GET" action="">   
         Username: <input type="text" name="username" placeholder="Enter Username" /><br>
-        Password: <input type="password" name="password" placeholder="Enter Password" /><br>
-        <input type="submit" name = register" value= "Submit"/>
-        <p><?php
-            if(!empty($out_value)){
-                echo $out_value;
+        <input type="submit" name="retrieve" value="Submit"/>
+        <p><?php 
+            if(!empty($ret_value)){
+                echo $ret_value;
+            } else {
+                echo "No songs available";
             }
         ?></p>
     </form>
